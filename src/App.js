c705a3f968d93c75da347805ca06ecde794a2ce8
Vaneet Chanana <vaneetchanana98@gmail.com>
2023-06-24 13:02:32 +0530

    added react router to the project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Routes, Route, NavLink, Outlet, Link } from 'react-router-dom';
+
+function Layout() {
+  return (
+    <div>
+      <nav>
+        <Link to='/'>Home</Link>
+        <Link to='about'>About</Link>
+        <Link to='/contact'>Contact</Link>
+      </nav>
+      <Outlet />
+    </div>
+  )
+}
+
+function Home() {
+  return <h1>This is the homepage</h1>
+}
+
+function About() {
+  return <h1>This is the aboutpage</h1>
+}
+
+function Contact() {
+  return <h1>This is the contactpage</h1>
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path='about' element={<About />} />
+          <Route path='contact' element={<Contact />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
